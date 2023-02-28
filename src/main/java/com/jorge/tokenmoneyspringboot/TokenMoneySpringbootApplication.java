caad6fe08de2f2e81eb88db58ded9d0fb5046cbe
Jorge <george16180@live.com>
2023-02-28 13:28:03 -0500

    First commit

---
+++ src/main/java/com/jorge/tokenmoneyspringboot/TokenMoneySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jorge.tokenmoneyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenMoneySpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenMoneySpringbootApplication.class, args);
+    }
+
+}
